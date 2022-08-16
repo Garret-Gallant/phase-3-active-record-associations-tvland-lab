b3451a7450003326b9b21e64a7e46c18636275c8
Garret Gallant <gallanttechnological@gmail.com>
2022-08-16 13:31:48 -0600

    complete

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    # self.characters.map {|c| c.actor.full_name}
+    self.actors.map do |actor|
+      actor.full_name
+    end
+  end
 end
